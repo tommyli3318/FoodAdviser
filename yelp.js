47cb2411514befd83329e418ba2c193a7e945960
Tommy Li <tommyli3318@gmail.com>
2020-03-20 16:23:41 -0700

    move request handling to JS

--- yelp.js
+++ yelp.js
@@ -1,1 +1,21 @@
-document.body.innerHTML = "test";
+const key = process.env.YELP_API_KEY
+const proxy = "https://cors-anywhere.herokuapp.com/"; // Work around CORS error
+const endpoint = 'https://api.yelp.com/v3/businesses/search'
+
+const params = {
+    'term': 'starbucks',
+    'limit': 10,
+    'radius': 10000,
+    'location': 'pomona'
+}
+
+var requestObj = {
+    url: proxy + endpoint,
+    data: params,
+    headers: {'Authorization': `bearer ${key}`}
+}
+
+$.ajax(requestObj)
+    .done(response => {
+        console.log(response)
+    })
