2d122df647b9db3cde8864e653a8bab7f8e61291
Tommy Li <tommyli3318@gmail.com>
2020-03-24 14:34:48 -0700

    format html

--- yelp.js
+++ yelp.js
@@ -1,21 +1,32 @@
-const key = process.env.YELP_API_KEY
-const proxy = "https://cors-anywhere.herokuapp.com/"; // Work around CORS error
-const endpoint = 'https://api.yelp.com/v3/businesses/search'
+const key = 'w74MySzCEyDtQ5dUjKFrkdeAU5Y86Ed6Qp2qKpwGn4_Pj-9cd8hJXGEmwkiUiCzGeCUanEAHmwW8b27rQXBK2rZ1DHbJEJl0okOt9k0eMi9B__vsS_89_LWOvMFzXnYx';
+const proxy = 'https://cors-proxy-tommyli3318.herokuapp.com/'; // Work around CORS error
+const endpoint = 'https://api.yelp.com/v3/businesses/search';
 
 const params = {
     'term': 'starbucks',
     'limit': 10,
     'radius': 10000,
     'location': 'pomona'
-}
+};
 
 var requestObj = {
     url: proxy + endpoint,
     data: params,
-    headers: {'Authorization': `bearer ${key}`}
-}
+    headers: {'Authorization': `bearer ${key}`},
+    dataType: 'json'
+};
 
 $.ajax(requestObj)
     .done(response => {
-        console.log(response)
-    })
+        console.log(response);
+        formatHTML(response);
+    });
+
+
+function formatHTML(results) {
+    document.getElementById("app").innerHTML = `
+    <h1>Results (top ${results.businesses.length})</h1>
+    ${results}
+    
+    `
+}
